0312d668dbea7112d384f2ac8e710a92e8fb3924
zwonkim <k980912@naver.com>
2023-11-16 21:30:03 +0900

    style: 모바일 레이아웃 수정

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,4 @@
-import { qnaList, resultList } from './data';
+import { qnaList, resultList } from "./data";
 
 const startPage = document.getElementById("startPage") as HTMLElement;
 const qnaPage = document.getElementById("qnaPage") as HTMLElement;
@@ -6,6 +6,7 @@ const resultPage = document.getElementById("resultPage") as HTMLElement;
 const startBtn = document.getElementById("startBtn") as HTMLButtonElement;
 const retryBtn = document.querySelector(".retryBtn") as HTMLButtonElement;
 const shareTwBtn = document.querySelector(".shareTw") as HTMLButtonElement;
+
 let selected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 retryBtn.addEventListener("click", () => {
@@ -30,7 +31,6 @@ shareTwBtn.addEventListener("click", () => {
 
 const setResult = () => {
   const result = calcResult();
-  // console.log("결과", result);
 
   const title = document.querySelector(".resultType") as HTMLDivElement;
   title.innerHTML = resultList[result].name;
@@ -48,12 +48,13 @@ const setResult = () => {
   }
 };
 
-type TypeCalcResult = () => number
+type TypeCalcResult = () => number;
 
-const calcResult:TypeCalcResult = () => {
+const calcResult: TypeCalcResult = () => {
   const max = selected.indexOf(Math.max(...selected));
-  // console.log("맥스", max);
+
   let index = -1;
+
   for (let i = 0; i < resultList.length; i++) {
     if (resultList[i].type.includes(max)) {
       index = i;
@@ -64,6 +65,7 @@ const calcResult:TypeCalcResult = () => {
 
 const goResult = () => {
   qnaPage.style.animation = "fadeOut 0.7s";
+
   setTimeout(() => {
     resultPage.style.animation = "fadeIn 0.7s";
     setTimeout(() => {
@@ -76,31 +78,37 @@ const goResult = () => {
   setResult();
 };
 
-const clickAnswer = (index:number) => {
-  const answers:NodeListOf<HTMLButtonElement> = document.querySelectorAll(".answerList");
+const clickAnswer = (index: number) => {
+  const answers: NodeListOf<HTMLButtonElement> =
+    document.querySelectorAll(".answerList");
+
   for (let i = 0; i < answers.length; i++) {
     answers[i].disabled = true;
     answers[i].style.animation = "fadeOut 0.9s";
+
     setTimeout(() => {
       answers[i].style.display = "none";
     }, 600);
   }
 };
 
-const createAnswer = (answer:string, index:number, type:number[]) => {
+const createAnswer = (answer: string, index: number, type: number[]) => {
   const aBox = document.querySelector(".answers");
   const answerBtn = document.createElement("button");
+
   answerBtn.classList.add("answerList");
   answerBtn.innerHTML = answer;
   aBox?.append(answerBtn);
 
   answerBtn.addEventListener("click", () => {
     clickAnswer(index);
+
     for (let i = 0; i < type.length; i++) {
       selected[type[i] - 1] += 1;
     }
-    // console.log(selected);
+
     index += 1;
+
     setTimeout(() => {
       goNext(index);
     }, 600);
@@ -114,13 +122,13 @@ const goNext = (index: number) => {
   }
 
   const qBox = document.querySelector(".question");
-  if(qBox !== null) {
+  if (qBox !== null) {
     qBox.innerHTML = qnaList[index].q;
-  for (let i = 0; i < qnaList[index].a.length; i++) {
-    const answer = qnaList[index].a[i].answer;
-    const type = qnaList[index].a[i].type;
-    createAnswer(answer, index, type);
-  }
+    for (let i = 0; i < qnaList[index].a.length; i++) {
+      const answer = qnaList[index].a[i].answer;
+      const type = qnaList[index].a[i].type;
+      createAnswer(answer, index, type);
+    }
   }
 
   const status = document.querySelector(".status") as HTMLDivElement;
@@ -128,15 +136,16 @@ const goNext = (index: number) => {
 };
 
 const getStart = () => {
-  if(startPage !== null && qnaPage !== null) {
+  if (startPage !== null && qnaPage !== null) {
     startPage.style.animation = "fadeOut 0.7s";
-  setTimeout(() => {
-    qnaPage.style.animation = "fadeIn 0.7s";
+
     setTimeout(() => {
-      startPage.style.display = "none";
-      qnaPage.style.display = "flex";
+      qnaPage.style.animation = "fadeIn 0.7s";
+      setTimeout(() => {
+        startPage.style.display = "none";
+        qnaPage.style.display = "flex";
+      }, 200);
     }, 200);
-  }, 200);
   }
   let index = 0;
   goNext(index);
